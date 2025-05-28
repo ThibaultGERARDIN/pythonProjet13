7e51f64f2f607010e6000fcd35ba5fdbaffbb2d5
Thibault G <thibault.gerardin@gmail.com>
2025-05-28 17:07:23 +0200

    refactorisation en 3 apps terminée, correction des quelques erreurs flake8 et inversion flèche bouton back dans le template letting

---
+++ lettings/migrations/0002_copy_data.py
@@ -0,0 +1,37 @@
+from django.db import migrations
+
+def copy_lettings_data(apps, schema_editor):
+    OldAddress = apps.get_model('oc_lettings_site', 'Address')
+    OldLetting = apps.get_model('oc_lettings_site', 'Letting')
+    NewAddress = apps.get_model('lettings', 'Address')
+    NewLetting = apps.get_model('lettings', 'Letting')
+
+    address_map = {}
+
+    for old_addr in OldAddress.objects.all():
+        new_addr = NewAddress.objects.create(
+            number=old_addr.number,
+            street=old_addr.street,
+            city=old_addr.city,
+            state=old_addr.state,
+            zip_code=old_addr.zip_code,
+            country_iso_code=old_addr.country_iso_code,
+        )
+        address_map[old_addr.id] = new_addr
+
+    for old_letting in OldLetting.objects.all():
+        NewLetting.objects.create(
+            title=old_letting.title,
+            address=address_map[old_letting.address_id],
+        )
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lettings', '0001_initial'),
+        ('oc_lettings_site', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(copy_lettings_data),
+    ]
